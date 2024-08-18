e80c365e2886ed117d924272f3213ee3a9bf1d97
Pasupathi Palanisamy <pasupathipalanisamy@Pasupathis-MacBook-Air.local>
2024-08-18 17:24:07 +0100

    Add validation for special char

--- src/main/java/pasu/projects/palindrome/Application.java
+++ src/main/java/pasu/projects/palindrome/Application.java
@@ -2,14 +2,10 @@ package pasu.projects.palindrome;
 
 import java.util.Objects;
 import java.util.Scanner;
-import java.io.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pasu.projects.palindrome.model.Input;
 
 @SpringBootApplication
 public class Application {
@@ -29,9 +25,20 @@ public class Application {
 		//Get input string - which is not null
 		String inputString;
 		do {
-			System.out.println("Enter your String to check if the string entered is Palindrome: ");
-			inputString = scanner.nextLine();
-			log.info(inputString);
+			System.out.println("Enter your word or phrase: ");
+			while (true){
+				inputString = scanner.nextLine();
+				if (!inputString.matches("[a-zA-Z0-9 ]+")){
+					System.out.println("please dont enter any special characters!!");
+					continue;
+				} else if ((inputString.length() > 100)){
+					System.out.println("Long string");
+					continue;
+				} else {
+					log.info("your word is {} , ", inputString);
+					break;
+				}
+			}
 		}
 		while (Objects.requireNonNull(inputString).isEmpty());
 
@@ -48,10 +55,3 @@ public class Application {
 	}
 }
 
-//	@Bean
-//  CommandLineRunner getInputValue(String inputString) {
-//		return args -> {
-//			Input input = new Input(1, inputString);
-//            log.info("Input String is : {}", input);
-//		};
-//	}
--- src/main/java/pasu/projects/palindrome/model/User.java
+++ src/main/java/pasu/projects/palindrome/model/User.java
@@ -1,23 +1,3 @@
 package pasu.projects.palindrome.model;
 
-public class User {
-    private int id;
-    private String username;
-
-    public User(int id, String username) {
-        this.id = id;
-        this.username = username;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-}
+public record User(Integer id, String username) { }
